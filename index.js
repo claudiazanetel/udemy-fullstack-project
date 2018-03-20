ff6e8f2714bd0c76470938bbfb702393f7f770c9
Claudia Zanetel <claudia.zanetel@gmail.com>
2018-03-20 17:57:48 +0100

    config folder

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const app = express();
 
-//154517560877-lcjnf537v3sgddc6msmcpov981fnm3cv.apps.googleusercontent.com
-//kM452XkJG5uyzDoKobHSYuLS
+const app = express();
 
 passport.use(new GoogleStrategy());
 
