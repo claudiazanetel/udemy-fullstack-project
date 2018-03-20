65eace30b6c233f36491a480fe5422992c142cbf
Claudia Zanetel <claudia.zanetel@gmail.com>
2018-03-20 17:48:10 +0100

    installed and required passport and passport strategy

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-});
+//154517560877-lcjnf537v3sgddc6msmcpov981fnm3cv.apps.googleusercontent.com
+//kM452XkJG5uyzDoKobHSYuLS
+
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
