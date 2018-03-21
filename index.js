ad0c99ccfc1f16e2a92932568500e68b582b2730
Claudia Zanetel <claudia.zanetel@gmail.com>
2018-03-21 09:57:03 +0100

    passport callback

--- index.js
+++ index.js
@@ -10,11 +10,23 @@ passport.use(new GoogleStrategy(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     },
-    (accessToken) => {
-        console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access Token', accessToken);
+        console.log('refresh Token', refreshToken);
+        console.log('profile', profile);
     }
     )
 );
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+            scope: ['profile', 'email']
+        }
+    )
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
